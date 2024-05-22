5c7c8908d9bcaec7c8736691193a874cc3032dba
Bevil56 <thanhvinhll56@gmail.com>
2024-05-22 08:23:41 +0700

    Adjust interface color scheme and align elements for optimal coherence.

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,7 +1,1 @@
-document.addEventListener("DOMContentLoaded", function () {
-  const sliderContainer = document.querySelector(".slider-container");
-  sliderContainer.addEventListener("wheel", function (event) {
-    event.preventDefault();
-    sliderContainer.scrollLeft += event.deltaY;
-  });
-});
+
